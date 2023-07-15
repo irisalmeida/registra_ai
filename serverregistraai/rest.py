a21561e17f289f15f25127c2fb44e8effe1fd662
Íris Almeida <054iris@gmail.com>
2023-07-15 20:34:40 -0300

    Adiciona asserts

--- serverregistraai/rest.py
+++ serverregistraai/rest.py
@@ -2,8 +2,22 @@ from flask import Flask, jsonify, request, make_response, abort, send_file
 
 app = Flask(__name__)
 
+balance = 0
 
-balance = 10
+
+def _assert(condition, status_code, message):
+    if condition: return
+    _abort(status_code, message)
+
+
+def _abort(status_code, message):
+    res = {
+        "message": message,
+        "status_code": status_code
+    }
+    response = make_response(jsonify(res), status_code)
+    abort(response)
+
 
 @app.route("/balance")
 def get_balance():
@@ -14,16 +28,43 @@ def get_balance():
 @app.route("/gain", methods=["POST"])
 def register_gain():
     global balance
+
     data = request.get_json()
+
+    _assert(data is not None, 400, "Faltou o body da requisição")
+    _assert("amount" in data, 400, "Body sem amount")
+
     amount = data.get("amount")
+    _assert(amount >= 0, 400, f"'amount' inválido: deve ser um valor positivo")
+
     balance += amount
-    return f"Ganho registrado! Agora você tem R${balance:.2f}"
+
+    res = {
+        "message": f"Ganho registrado! Agora você tem R${balance}",
+        "registered_gain": amount,
+        "balance": balance
+    }
+    return jsonify(res), 200
 
 
 @app.route("/expense", methods=["POST"])
 def register_expense():
     global balance
+
     data = request.get_json()
+
+    _assert(data is not None, 400, "Faltou o body da requisição")
+    _assert("amount" in data, 400, "Body sem amount")
+    
     amount = data.get("amount")
+    _assert(amount >= 0, 400, f"'amount' inválido: deve ser um valor positivo")
+    
     balance -= amount
-    return f"Gasto registrado! Agora você tem R${balance:.2f}"
+    
+    res = {
+        "message": f"Gasto registrado! Agora você tem R${balance}",
+        "registered_expense": amount,
+        "balance": balance
+    }
+    return jsonify(res), 200
+  
