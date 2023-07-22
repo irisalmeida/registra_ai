95eaa4ebb6357f92e81254066554dac23b61399b
Iago Tito <38673719+iagotito@users.noreply.github.com>
2023-07-22 15:02:17 -0300

    refactor: conexão com baco de dados e classe Record (#13)

--- registraai/models.py
+++ registraai/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
+from typing import Optional
 
 
 class Record():
-    def __init__(self, amount:float, description:str) -> None:
+    def __init__(self, amount:float, description:str, ts:Optional[datetime]=None) -> None:
         self.amount = amount
         self.description = description
-        self.timestamp = datetime.now()
+        self.ts = datetime.now() if ts is None else ts
 
     def to_dict(self) -> dict:
         rec = vars(self).copy()
--- registraai/sql/create_table_records.sql
+++ registraai/sql/create_table_records.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS records (
+  id SERIAL PRIMARY KEY,
   amount float,
   description varchar,
   ts timestamp
